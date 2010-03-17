2072dec86fdd192177b646b719f32d22aea2a63d
Calogero Lo Leggio <kalos@nerdrug.org>
2010-03-17 16:50:46 +0100

    APT: fix attributes

--- apt/attributes/apt.rb
+++ apt/attributes/apt.rb
@@ -1,14 +1,7 @@
 default[:apt][:unattended_upgrades] = true
-<<<<<<< HEAD
-default[:apt][:cache_limit] = "150000000"
-default[:apt][:mirrors] = [ "mir1.ovh.net", "ftp.fr.debian.org", "ftp.de.debian.org" ]
-default[:apt][:extra_sources] = [ "backports", "opscode" ]
-default[:apt][:email_admin] = "root@nerdrug.org"
-=======
 default[:apt][:cache_limit]         = "150000000"
-default[:apt][:mirrors]             = [ "ftp.it.debian.org", "ftp.de.debian.org" ]
+default[:apt][:mirrors]             = [ "mir1.ovh.net", "ftp.fr.debian.org", "ftp.de.debian.org" ]
 default[:apt][:extra_sources]       = [ "testing", "backports", "opscode" ]
 default[:apt][:sources]             = false
-default[:apt][:email_admin]         = "sistemistica@teleunit.it"
+default[:apt][:email_admin]         = "root@nerdrug.org"
 default[:apt][:proxy_url]           = ""
->>>>>>> 0b8cbb5... APT recipe: added apt-proxy
